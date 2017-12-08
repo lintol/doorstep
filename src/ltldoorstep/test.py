64144932124cb9dea4b09c76087acdf39c11a3b0
becca1993 <rebeccalmartin16@gmail.com>
2017-12-08 19:23:24 +0000

    Adding new processors
    These are the new processors that I have written, for the CSV checker, two GeoJSON checkers (wasn't sure which version to include so included both) and a PII Checker.

--- src/ltldoorstep/test.py
+++ src/ltldoorstep/test.py
@@ -1,6 +1,54 @@
-from flask import Flask
-app = Flask(__name__)
+from piianalyzer.analyzer import PiiAnalyzer
+import logging
+import numpy as np
+import pandas as p
+import sys
+pii_details = {
+    'N': 'name',
+    'P': 'phone_no',
+    'A': 'address',
+    'C': 'credit_card_no',
+    'E': 'email_address',
+    'O': 'organization'
 
-@app.route('/')
-def hello_world():
-        return 'Hello, World!'
+}
+
+# This function will return the results of a csv file to find personally identifable info(PII)
+def return_report(csv):
+
+    piianalyzer = PiiAnalyzer(csv)  # Feeding in csv file to PIIAnalyzer...
+
+    dataset = set()  # set dataset object to set method
+
+    analysis = piianalyzer.analysis()  # Assigning the results of the analysis to analysis var...
+   
+    report = {}  # setting up report dict....
+
+    np.vectorize(lambda cell: dataset.update({analysis(a)for a in cell}))
+
+    return {
+        'check_pii_detail:pii-found': ('PII details found...', logging.INFO, [pii_details[d[0]] for d in dataset])
+    }
+
+ 
+# This function will return the workflow taken from return_report, feeds a csv file into return_report
+def get_workflow(filename):
+    workflow = {  # Setting up workflow dict...
+        'loading': (p.read_csv, filename), # Using Pandas library to read csv file passed in, and also pass the filename
+        'report_returned': (return_report, 'loading'), # Using return_report and loading csv file....
+        'output': (list, ['report_returned']) # This will be the output
+    }
+    return workflow  # Returns workflow dict
+
+if __name__ == "__main__":
+    argv = sys.argv
+    print(return_report(argv[0]))
+
+
+
+
+
+
+
+
+
