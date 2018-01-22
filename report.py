e94ab601fe38be45d3d6385faae71bf0fc9cca1e
becca1993 <rebeccalmartin16@gmail.com>
2018-01-22 10:29:09 +0000

    Updating Report class
    Adding some suggested changes to the Report class and associated subclasses.

--- report.py
+++ report.py
@@ -23,17 +23,31 @@ class Report(object):
 
 class TabularReport(Report):
 
-	
-    def __init__(self):
-        super().__init__(self.processor, self.info, self.logging, self.location)
-        super().add_issue(name, processor, info, logging.WARNING, location)
+    """Class for forming reports in a tabular form."""
 
+	
+    def add_issue(self, name, processor, info, logging, location):
+        issue(
+            processor,
+            logging.WARNING,
+            info,
+            _("test") + ': ' + str(mismatching_columns),
+            error_data={'mismatching-columns': mismatching_columns}
+        )
 
 class GeoJSONReport(Report):
-    def __init__(self):
-        super().__init__(self.processor, self.info, self.logging, self.location)
-        super().add_issue(name, processor, info, logging.WARNING, location)
 
+    """Class for forming reports in a GeoJSON form."""
+
+
+    def add_issue(self, name, processor, info, logging, location):
+        issue(
+            processor,
+            logging.WARNING,
+            info,
+            _("test") + ': ' + str(mismatching_columns),
+            error_data={'mismatching-columns': mismatching_columns}
+        )
 
 
 
