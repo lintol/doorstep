c5e6ed08a7eab017c5f17f58c4e889e9091a9a83
becca1993 <rebeccalmartin16@gmail.com>
2018-02-07 23:45:09 +0000

    upload wip version of boundary checker and report
    wip report and boundary checker

--- report.py
+++ report.py
@@ -1,55 +1,46 @@
 import logging
 """This is the report object that will be used for the standardisation of processors reporting
 The Report superclass can be inherited for different forms of reporting i.e. tabluar, GeoJSON etc."""
-class Report(object):
-	
-	def __init__(self, processor, info, logging, location):
-            self.processor = processor
-            self.info = info
-            self.logging = logging
-            self.location = location
 
-    def add_issue(self, name, processor, info, logging, location):
-        issue(
-            processor,
+
+class Report(object):
+
+    def __init__(self, processor, info, logging):
+        self.processor = processor
+        self.info = info
+        self.logging = logging.NONE
+
+    @staticmethod
+    def add_issue(name, info, logging):
+        return issue(
+            name,
             logging.WARNING,
             info,
             _("test") + ': ' + str(mismatching_columns),
             error_data={'mismatching-columns': mismatching_columns}
         )
-
-
 
 
 class TabularReport(Report):
-
-    """Class for forming reports in a tabular form."""
-
-	
-    def add_issue(self, name, processor, info, logging, location):
-        issue(
-            processor,
+    @staticmethod
+    def add_issue(name, info, logging):
+        return issue(
+            name,
             logging.WARNING,
             info,
             _("test") + ': ' + str(mismatching_columns),
             error_data={'mismatching-columns': mismatching_columns}
         )
+
+
 
 class GeoJSONReport(Report):
-
-    """Class for forming reports in a GeoJSON form."""
-
-
-    def add_issue(self, name, processor, info, logging, location):
-        issue(
-            processor,
+    @staticmethod
+    def add_issue(name, info, logging):
+        return issue(
+            name,
             logging.WARNING,
             info,
             _("test") + ': ' + str(mismatching_columns),
             error_data={'mismatching-columns': mismatching_columns}
         )
-
-
-
-
-		
